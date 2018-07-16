99e0a9e9ff0b3b3160d6ff8c14acb02c5766b62f
Duncan McColl <duncmc831@gmail.com>
2018-07-16 16:12:23 -0700

    add: view of linear when booting up.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import GraphSimilarity from "./GraphSim.js"
-import data from "./data/t-true-cheat"
+import data from "./data/l-true-cheat"
 
 
 class App extends Component {
   render() {
-    console.log(data)
     return (
       <GraphSimilarity graphL={data.graphL} graphR={data.graphR} graphSim={data.sim} alignments={data.alignments}/>
   );
